5f74bbf7dd8874b38525aabcdab1e74fc28162c7
raed saidi <raed.saidi@insat.ucar.tn>
2025-02-01 23:52:46 +0100

    hero section

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -1,14 +1,13 @@
 
-
+import Hero from '/src/components/Hero.jsx'
 import Navbar from '/src/components/Navbar.jsx'
 function App() {
   
 
   return (
     <>
-    
     <Navbar />
-      
+    <Hero />
     </>
   )
 }
---
+++ real-estate/src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className="Hero">
+        <h1>We Provide Architectural design and Construction</h1>
+        <p>​More than 100 building and housing projects that we have built. The building owner chose us over other contractors in Jakarta,because our work is different</p>
+        <button>Discover More</button>
+       <ul>
+            <li>
+                <p className='item'>300
+                    <span>+</span>
+                    </p>
+                    <p className='description' >Happy Client</p>
+
+                    </li>
+                    <li>
+                <p className='item' >900
+                    <span>+</span>
+                    </p>
+                    <p className='description' >Amazing Projects</p>
+                    </li>
+                    <li>
+                <p className='item' >20
+                    <span>+</span>
+                    </p>
+                    <p className='description'>Awards Winning</p>
+                    </li>
+            
+       </ul>
+    </section>  
+  )
+}
+
+export default Hero
